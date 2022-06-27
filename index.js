1238cfdfe4e3dc270effeeb7b57e473e1503e753
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2022-06-27 14:20:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,23 +12,23 @@ const pino = require('pino')
 const axios = require('axios')
 const chalk = require('chalk')
 const CFonts = require('cfonts');
-const FileType = require('file-type')
 const yargs = require('yargs/yargs')
+const FileType = require('file-type')
 const { Boom } = require('@hapi/boom')
-const { Collection, Simple, Store } = require("./lib")
-const { MakeSession } = require('./lib/auth')
+const { MakeSession } = require('./lib/waconnect/auth')
 const PhoneNumber = require('awesome-phonenumber')
+const { Collection, Simple, Store } = require("./lib")
 const { exec, spawn, execSync } = require("child_process")
 const config = JSON.parse(fs.readFileSync('./config.json'))
 const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./lib/exif')
 const { smsg, isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep } = require('./lib/myfunc')
 // Jimbru session
-global.authFile ='./Jimbru.json'
-if(!fs.existsSync('./Jimbru.json')){
+global.authFile ='./auth.json'
+if(!fs.existsSync('./auth.json')){
     MakeSession(global.session,authFile)
     }
 setTimeout(() => {    
-const { state, saveState } = useSingleFileAuthState(`./session.json`)
+const { state, saveState } = useSingleFileAuthState(`./auth.json`)
 global.api = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name] } : {}) })) : '')
 // MEMORY STORE 
 const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
