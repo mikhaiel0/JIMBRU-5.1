5d4534d74d60487ceadd3742f94e6fa76fcd930f
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2022-06-27 17:30:50 +0530

    Update Jimbru.js

--- Jimbru.js
+++ Jimbru.js
@@ -6,18 +6,33 @@
 process.on('uncaughtException', console.error) //Error log
 require("./config")
 const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType, WAFlag } = require('@adiwajshing/baileys')
+const fs = require('fs')
 const os = require('os')
 const util = require('util')
 const path = require('path')
 const primbon = new Primbon()
 const axios = require('axios')
+const { wikiSearch } = require('./lib/wiki.js');
 const chalk = require('chalk')
-const haha = require('xfarr-api')
+const haha = require('xfarr-api') //
+const hx = require("hxz-api") //
+const { EmojiAPI } = require("emoji-api")
+const emoji = new EmojiAPI()
+const hxz = require('./lib/hxz-api') //
+const bdr = require('rumus-bdr') //
+const yogipw = require("tod-api") //
 const { JSDOM } = require('jsdom')
+const imgbbUploader = require('imgbb-uploader')
 const speed = require('performance-now')
 const moment = require('moment-timezone')
 const { performance } = require('perf_hooks')
 const { Primbon } = require('scrape-primbon')
+const primbon = new Primbon()
+let fetch = require('node-fetch')
+const ID3Writer = require('browser-id3-writer')
+const { isLimit, limitAdd, getLimit, giveLimit, addBalance, kurangBalance, getBalance, isGame, gameAdd, givegame, cekGLimit } = require('./lib/limit.js');
+// const { TiktokDownloader } = require('./lib/tiktokdl') 
 const { exec, spawn, execSync } = require("child_process")
+const { Sticker, createSticker, StickerTypes } = require('wa-sticker-formatter')
 const { aiovideodl, savefrom, instagramdl, instagramdlv2, instagramdlv3 } = require('@bochilteam/scraper')
 const { smsg, formatp, tanggal, formatDate, getTime, isUrl, sleep, clockString, runtime, fetchJson, getBuffer, jsonformat, format, parseMention, getRandom } = require('./lib/myfunc')
